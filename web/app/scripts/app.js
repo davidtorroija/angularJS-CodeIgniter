2ef376d749aeb17e244fbf78c3f5d0d1b45fea54
David Torroija <david.torroija@provedatos.com>
2013-11-18 09:51:44 -0500

    commit

--- web/app/scripts/app.js
+++ web/app/scripts/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('mainApp', [
   'ngCookies',
   'ngResource'
--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
-'use strict';
-
 angular.module('mainApp')
   .controller('MainCtrl', function ($scope) {
+	'use strict';
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    // a = 1;
   });
